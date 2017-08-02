13e4c9c66432937720fe8734f80f4e191d8f6f3a
projectmaking123 <jason.89.wang@gmail.com>
2017-08-02 16:19:48 -0400

    added cors

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to my express backend api server to call on the forecast api. I made this server to bypass the CORS prevention.
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const request = require('request');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get('/:lat/:lng?', function(req, res, next) {
   request({
